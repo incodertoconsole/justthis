571b74967482ab8e07b533449c4714f0f56647fa
incodertoconsole <aaaaxmadali2009incoder@gmail.com>
2022-12-08 23:44:01 +0300

    second commit

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ let users = [
     { names: "Salimbek", age: 17 },
     { names: "Abduqahhor", age: 23 }
 ]
+
 let getarray = users;
 console.log(getarray);
-let count = 0;
 
-for (let i = 0; i <= users.length; i++) {
+let count = false;
+
+for (let i = 0; i < users.length; i++) {
     let getName = users[i].names.slice(0, username.length);
     if (getName == username) {
         console.log(users[i].names);
+        count = true;
         break;
     }
+}
+
+if (count == false) {
+    console.log("This name is not founded!");
 }
